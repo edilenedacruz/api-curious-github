f0f4fb351b43821a0db7c5ee9d9156bf04739b80
Edilene <edilene_cruz@yahoo.com.br>
2017-03-21 13:44:18 -0600

    implements authentication w/o gem, figaro was set up

--- Gemfile
+++ Gemfile
@@ -32,10 +32,13 @@ gem 'jbuilder', '~> 2.5'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+gem 'faraday'
+gem 'figaro'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
+  gem 'pry'
 end
 
 group :development do
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_user, :authorize!
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def authorize!
+    unless current_user
+      redirect_to root_path
+    end
+  end
 end
