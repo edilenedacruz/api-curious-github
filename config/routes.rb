b944ccbe10c1005b80b7bd3052a82271f575ceb8
Edilene <edilene_cruz@yahoo.com.br>
2017-03-25 23:18:25 -0600

    updated gitignore

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   root "home#index"
-  # get "/login/oauth/authorize", to: 'dashboard#index'
   get '/auth/github/callback', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
-
-
+  
   namespace :user, path: ':user' do
     get '/dashboard', to: 'dashboard#index'
     get '/stars', to: 'stars#index'
@@ -13,14 +11,5 @@ Rails.application.routes.draw do
     get '/repos', to: 'repos#index'
     get '/organizations', to: 'organizations#index'
     get '/events', to: 'events#index'
-
-
-  # resources :dashboard, only: [:index]
-  # resources :stars, only: [:index]
-  # resources :followers, only: [:index]
-  # resources :following, only: [:index]
-  # resources :repos, only: [:index]
-  # resources :organizations, only: [:index]
-  # resources :events, only: [:index]
   end
 end
